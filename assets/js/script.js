eae047c6bc1e9d32714e490260b60578471d9a66
Amy Nikitas <amy.nikitas@gmail.com>
2022-03-26 17:48:39 -0400

    updates

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=bcbc10570516f3605acc117ec4843bb6')
+//         console.log(data)
+//     return data.json();
+
