f60d6ea00c93ebe6639f483bfa80b76461074af3
Your Name <amy.nikitas@gmail.com>
2022-03-27 03:41:24 -0400

    more additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-// fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=bcbc10570516f3605acc117ec4843bb6')
-//         console.log(data)
-//     return data.json();
+let pastSearchEl = document.querySelector("#past-searches")
 
+
+
+
+
+
+
+
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=bcbc10570516f3605acc117ec4843bb6').then( function(data){
+    //console.log(data)
+    return data.json();
+ }).then(function(completeData){
+     console.log(completeData);
+ })
+
+let APIKey = "bcbc10570516f3605acc117ec4843bb6";
